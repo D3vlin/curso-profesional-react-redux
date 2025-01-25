0c3f723c434de6e3bd3dd9f14f25436fb007cc85
D3vlin <alexisdq16@gmail.com>
2025-01-25 08:22:02 -0500

    add logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Col } from 'antd';
 import './App.css'
+import logo from './statics/logo.svg'
 import { Searcher } from './Components/Searcher';
 import { PokemonList } from './Components/PokemonList';
 
 function App() {
   return (
     <div className="App">
-      <Col span={8} offset={8}>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='pokedux' />
         <Searcher />
       </Col>
       <PokemonList />
