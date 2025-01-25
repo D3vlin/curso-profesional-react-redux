4cc72bcd179183641ec336ac990382366de181f5
D3vlin <alexisdq16@gmail.com>
2025-01-25 09:34:53 -0500

    using redux devtools extension

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const store = legacy_createStore(pokemonsReducer)
+const store = legacy_createStore(pokemonsReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 root.render(
   <React.StrictMode>
